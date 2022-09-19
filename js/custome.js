3891e52767f1ec85c6e4daf1eaed6022cf87f834
mahbubulhassan42 <mahbubulhassan42@gmail.com>
2022-09-19 22:19:30 +0600

    boom

---
+++ js/custome.js
@@ -0,0 +1,23 @@
+$('.team_slide').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  });
+
+   $('.single-item').slick({
+       autoplay: true,
+       autoplaySpeed: 2000,
+       arrows: false,
+   });
+
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+
+
+                  
